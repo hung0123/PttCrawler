ba5235098073db3a847a55ae9c032aeddfe8f7db
李紘紘 <flyking1486325@gmail.com>
2020-12-12 11:18:52 +0800

    增加自我介紹頁

---
+++ Controllers/IntroController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PttCrawler.Controllers
+{
+    public class IntroController:Controller
+    {
+        public ActionResult Intro()
+        {
+            return View();
+        }
+    }
+}
